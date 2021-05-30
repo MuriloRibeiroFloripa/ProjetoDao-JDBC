e8ce528f3fc4078fc0c6413a6951d47731d37b18
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-05-30 11:51:14 -0300

    Reusando a Instanciação

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -4,20 +4,17 @@ import negocio.dao.FabricaDao;
 import negocio.dao.VendedorDao;
 import negocio.entidade.Vendedor;
 
-/*
- * Incremento de teste do projeto
- */
-
 public class Programa {
 
 	// Metodo Principal
 	public static void main(String[] args) {
 		
+		// instanciado a fabricaDao
 		VendedorDao vendedorDao = FabricaDao.criarVendedorDao();
 		
 		// busca vendedor pelo id
-		Vendedor vendedor = vendedorDao.buscaPorId(2);
-		
+		System.out.println("======== Teste 1: Vendedor Busca Por ID ========");
+		Vendedor vendedor = vendedorDao.buscaPorId(2);		
 		System.out.println(vendedor);
 		}
 }
--- src/negocio/dao/implementacao/VendedorDaoImplentacaoJDBC.java
+++ src/negocio/dao/implementacao/VendedorDaoImplentacaoJDBC.java
@@ -12,7 +12,6 @@ import negocio.dao.VendedorDao;
 import negocio.entidade.Departamento;
 import negocio.entidade.Vendedor;
 
-
 /*
  *  Estruturação do DAO implementando a classe Vendedor DAO JDBC
  * Criando o DAO Factory, classe responsavel por instanciar os DAOs.
@@ -25,27 +24,28 @@ public class VendedorDaoImplentacaoJDBC implements VendedorDao {
 	// Implementação da dependecia do DAO com a conexão
 	// Para ter a coneção a disposição nesta classe.
 	private Connection conn;
+
 	public VendedorDaoImplentacaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
-	
+
 	// Implementação dos metodos
 	@Override
 	public void inserir(Vendedor obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void atualizar(Vendedor obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void apagarPorId(Vendedor id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -54,33 +54,31 @@ public class VendedorDaoImplentacaoJDBC implements VendedorDao {
 		ResultSet rs = null; // Tras os dados em formato tabela
 		try {
 			st = conn.prepareStatement(
-					"SELECT vendedor.*,departamento.Nome as DepNome "
-					+ "FROM vendedor INNER JOIN departamento "
-					+ "ON vendedor.DepartamentoId = departamento.Id "
-					+ "WHERE vendedor.Id = ?");
-			
+					"SELECT vendedor.*,departamento.Nome as DepNome " + "FROM vendedor INNER JOIN departamento "
+							+ "ON vendedor.DepartamentoId = departamento.Id " + "WHERE vendedor.Id = ?");
+
 			st.setInt(1, id);
 			rs = st.executeQuery();
 			// associação dos objetos, rs aponta para posição zero da tabela
-			if(rs.next()) { // testa se veio algum resultado
-				//Implementação do metodo para retornar o vendedor por Id.
-				Departamento dep = new Departamento();
-				dep.setId(rs.getInt("DepartamentoId"));
-				dep.setNome(rs.getString("DepNome"));
-				Vendedor obj = new Vendedor();
-				obj.setId(rs.getInt("Id"));
-				obj.setNome(rs.getString("Nome"));
-				obj.setEmail(rs.getString("Email"));
-				obj.setBaseSalarial(rs.getDouble("BaseSalario"));
-				obj.setdataNascimento(rs.getDate("DataNascimento"));
-				obj.setDepartamento(dep);
-				return obj;				
+			if (rs.next()) { // testa se veio algum resultado
+
+				// chamando a função de Instanciar Departamento
+				Departamento dep = instanciaDepartamento(rs);
+
+				// Chama a função de instanciar o Vendedor passando
+				// rs do PreparedStatement=rs e o dep da instanciaDepartamento ja instanciado
+				// acima.
+				Vendedor obj = instanciaVendedor(rs, dep);
+
+				// retorna os objetos instanciados pela chamada de função.
+				return obj;
 			}
 			return null;
-			
+
+			// Capturando SQLexception, lanca exception personalizada.
 		} catch (SQLException e) {
-			throw new DbException(e.getMessage()); //lança exceção personalizada
-		} 
+			throw new DbException(e.getMessage()); // lança exceção personalizada
+		}
 		// fechando os recursos st, rs
 		finally {
 			DB.closeStatement(st);
@@ -88,10 +86,31 @@ public class VendedorDaoImplentacaoJDBC implements VendedorDao {
 		}
 	}
 
+	// Instanciar Vendedor com tratamento personalizado, propagando a SQLException
+	private Vendedor instanciaVendedor(ResultSet rs, Departamento dep) throws SQLException {
+
+		Vendedor obj = new Vendedor();
+		obj.setId(rs.getInt("Id"));
+		obj.setNome(rs.getString("Nome"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalarial(rs.getDouble("BaseSalario"));
+		obj.setdataNascimento(rs.getDate("DataNascimento"));
+		obj.setDepartamento(dep);
+		return obj;
+	}
+
+	// Instanciar Departamento com tratamento personalizado, propagando a
+	// SQLException
+	private Departamento instanciaDepartamento(ResultSet rs) throws SQLException {
+		Departamento dep = new Departamento();
+		dep.setId(rs.getInt("DepartamentoId"));
+		dep.setNome(rs.getString("DepNome"));
+		return dep;
+	}
+
 	@Override
 	public List<Vendedor> buscaTodos() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
