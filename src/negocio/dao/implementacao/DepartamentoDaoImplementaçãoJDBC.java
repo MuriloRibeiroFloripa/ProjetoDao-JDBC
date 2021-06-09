459bdcdcda237f2b190e4887beec42ce3ed26979
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-06-08 22:43:16 -0300

    Implementação departamento dao JDBC busca por Id

--- src/negocio/dao/implementacao/DepartamentoDaoImplementaçãoJDBC.java
+++ src/negocio/dao/implementacao/DepartamentoDaoImplementaçãoJDBC.java
@@ -1,5 +1,78 @@
 package negocio.dao.implementacao;
 
-public class DepartamentoDaoImplementaçãoJDBC {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import db.DB;
+import db.DbException;
+import negocio.dao.DepartamentoDao;
+import negocio.entidade.Departamento;
+
+public class DepartamentoDaoImplementaçãoJDBC implements DepartamentoDao{
+	
+	// Implementação da dependecia do DAO com a conexão
+	// Para ter a coneção a disposição nesta classe.
+	private Connection conn;
+	
+	public DepartamentoDaoImplementaçãoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+
+	// Implementação dos metodos
+	@Override
+	public void inserir(Departamento obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(Departamento obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void apagarPorId(Departamento id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Departamento buscaPorId(Integer id) {
+
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+				"SELECT * FROM departamento WHERE Id = ?");
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if (rs.next()) {
+				Departamento obj = new Departamento();
+				obj.setId(rs.getInt("Id"));
+				obj.setNome(rs.getString("Nome"));
+				return obj;
+			}
+			return null;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+
+	@Override
+	public List<Departamento> buscaTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
